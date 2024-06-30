ce39c17dc86ebac3ea053d68286ca38cb0c4056d
Oyyanv <muhammadroyyan527@gmail.com>
2024-06-30 18:49:31 +0800

    section introduction

---
+++ src/app/component/pengenalan.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SectionPlace = () => {
+    return (
+        <section>
+            <div className="">
+                <div className="">
+                    <h1>I'm Muhammad Royyan</h1>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SectionPlace;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import SectionPlace from "./component/pengenalan";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>portofolio</h1>
+    <main className="">
+     <SectionPlace />
     </main>
   );
 }
