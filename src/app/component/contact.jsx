e3ae0ef077ba0d6b8270f2188c452f3e90b8df86
Oyyanv <muhammadroyyan527@gmail.com>
2024-11-22 15:22:24 +0800

    fixing card project

--- src/app/component/contact.jsx
+++ src/app/component/contact.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export const ContactSection = () =>{
     return(
         <section id="contact">
-            <h2 className="justify-center flex items-center">WIP</h2>
+            <div>
+                <h2 className="justify-center flex items-center">WIP</h2>
+            </div>
         </section>
     )
 }
--- src/app/component/project.jsx
+++ src/app/component/project.jsx
@@ -3,107 +3,118 @@ import React from "react";
 export const ProjectSection = () => {
     return(
         <section id="Project" className="xl:overflow-x-hidden">
-            <div className="md:mb-10 mb-3 text-4xl text-[#F9F9F9] font-Questrial underline font-semibold justify-center flex">
+            <div className="md:mb-10 mb-3 text-4xl text-[#F9F9F9] font-Questrial underline font-semibold justify-center flex" data-aos="fade-up"
+            data-aos-duration="2000">
         MY PROJECTS
       </div>
-            <div className="flex flex-nowrap justify-start xl:justify-center gap-6 p-10 w-screen overflow-scroll xl:overflow-x-auto">
-     {/* Card Component 1*/}
-        <div class="relative flex flex-col mt-12 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-52 xl:w-52">
-            <div
-                class="relative h-18 xl:h-28 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
-                <img
-                    src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
-                    alt="card-image" />
-            </div>
-            <div class="p-6">
-                <h5 class="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                    Kessaku Project
-                </h5>
-                <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                    Ecommerce Website
-                </p>
-            </div>
-            <div class="p-6 pt-0">
-                <button
-                    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
-                    type="button">
-                    Link
-                </button>
-            </div>
-        </div>
-        {/* Card Component 2*/}
-        <div class="relative flex flex-col mt-12 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-52 xl:w-52">
-            <div
-                class="relative h-18 xl:h-28 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
-                <img
-                    src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
-                    alt="card-image" />
-            </div>
-            <div class="p-6">
-                <h5 class="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                    Profile Pky
-                </h5>
-                <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                    Semantic Website
-                </p>
-            </div>
-            <div class="p-6 pt-0">
-                <button
-                    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
-                    type="button">
-                    Link
-                </button>
-            </div>
-        </div>
-        {/* Card Component 3*/}
-        <div class="relative flex flex-col mt-12 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-52 xl:w-52">
-            <div
-                class="relative h-18 xl:h-28 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
-                <img
-                    src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
-                    alt="card-image" />
-            </div>
-            <div class="p-6">
-                <h5 class="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                    Knowlege App
-                </h5>
-                <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                    Education App
-                </p>
-            </div>
-            <div class="p-6 pt-0">
-                <button
-                    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
-                    type="button">
-                    Link
-                </button>
-            </div>
-        </div>
-        {/* Card Component 3*/}
-        <div class="relative flex flex-col mt-12 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-52 xl:w-52">
-            <div
-                class="relative h-18 xl:h-28 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
-                <img
-                    src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
-                    alt="card-image" />
-            </div>
-            <div class="p-6">
-                <h5 class="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-                    Test
-                </h5>
-                <p class="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-                    Ecommerce Website
-                </p>
-            </div>
-            <div class="p-6 pt-0">
-                <button
-                    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
-                    type="button">
-                    Link
-                </button>
-            </div>
-        </div>
-        </div>
+      <div className="flex xl:justify-center xl:overflow-x-hidden">
+  <div className="flex flex-nowrap justify-start overflow-y-hidden  xl:justify-center xl:flex gap-6 p-10 xl:min-w-screen w-screen overflow-scroll xl:overflow-x-auto">
+    {/* Card Component 1 */}
+    <div className="relative flex flex-col mt-12 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-52 xl:w-52" data-aos="fade-up"
+            data-aos-duration="1900">
+      <div className="relative h-18 xl:h-28 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+        <img
+          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+          alt="card-image"
+        />
+      </div>
+      <div className="p-6">
+        <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+          Kessaku Project
+        </h5>
+        <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+          Ecommerce Website
+        </p>
+      </div>
+      <div className="p-6 pt-0">
+        <button
+          className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+          type="button"
+        >
+          Link
+        </button>
+      </div>
+    </div>
+    {/* Card Component 1 */}
+    <div className="relative flex flex-col mt-12 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-52 xl:w-52" data-aos="fade-up"
+            data-aos-duration="1800">
+      <div className="relative h-18 xl:h-28 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+        <img
+          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+          alt="card-image"
+        />
+      </div>
+      <div className="p-6">
+        <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+          Kessaku Project
+        </h5>
+        <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+          Ecommerce Website
+        </p>
+      </div>
+      <div className="p-6 pt-0">
+        <button
+          className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+          type="button"
+        >
+          Link
+        </button>
+      </div>
+    </div>
+    {/* Card Component 1 */}
+    <div className="relative flex flex-col mt-12 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-52 xl:w-52" data-aos="fade-up"
+            data-aos-duration="1700">
+      <div className="relative h-18 xl:h-28 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+        <img
+          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+          alt="card-image"
+        />
+      </div>
+      <div className="p-6">
+        <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+          Kessaku Project
+        </h5>
+        <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+          Ecommerce Website
+        </p>
+      </div>
+      <div className="p-6 pt-0">
+        <button
+          className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+          type="button"
+        >
+          Link
+        </button>
+      </div>
+    </div>
+    {/* Card Component 1 */}
+    <div className="relative flex flex-col mt-12 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-52 xl:w-52" data-aos="fade-up"
+            data-aos-duration="1600">
+      <div className="relative h-18 xl:h-28 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
+        <img
+          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+          alt="card-image"
+        />
+      </div>
+      <div className="p-6">
+        <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+          Kessaku Project
+        </h5>
+        <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
+          Ecommerce Website
+        </p>
+      </div>
+      <div className="p-6 pt-0">
+        <button
+          className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
+          type="button"
+        >
+          Link
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
         </section>
     )
 }
