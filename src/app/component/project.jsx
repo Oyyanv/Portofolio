d4454097d36ad7991f694847187031abba6b796d
Oyyanv <muhammadroyyan527@gmail.com>
2024-11-25 09:06:01 +0800

    fixing card project and page

--- src/app/component/project.jsx
+++ src/app/component/project.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ProjectSection = () => {
     return(
-        <section id="Project" className="xl:overflow-x-hidden">
+        <section id="Project" className="xl:overflow-x-hidden relative min-h-screen flex flex-col xl:items-center xl:justify-center md:items-center md:justify-center items-start justify-center">
             <div className="md:mb-10 mb-3 text-4xl text-[#F9F9F9] font-Questrial underline font-semibold justify-center flex" data-aos="fade-up"
             data-aos-duration="2000">
         MY PROJECTS
@@ -26,7 +26,7 @@ export const ProjectSection = () => {
           Ecommerce Website
         </p>
       </div>
-      <div className="p-6 pt-0">
+      <div className="p-6 pt-5">
         <button
           className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
           type="button"
@@ -35,7 +35,7 @@ export const ProjectSection = () => {
         </button>
       </div>
     </div>
-    {/* Card Component 1 */}
+    {/* Card Component 2 */}
     <div className="relative flex flex-col mt-12 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-52 xl:w-52" data-aos="fade-up"
             data-aos-duration="1800">
       <div className="relative h-18 xl:h-28 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
@@ -46,13 +46,13 @@ export const ProjectSection = () => {
       </div>
       <div className="p-6">
         <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-          Kessaku Project
+          Knowledge
         </h5>
         <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-          Ecommerce Website
+          Education App
         </p>
       </div>
-      <div className="p-6 pt-0">
+      <div className="p-6 pt-5">
         <button
           className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
           type="button"
@@ -72,13 +72,13 @@ export const ProjectSection = () => {
       </div>
       <div className="p-6">
         <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-          Kessaku Project
+          INFOKUS
         </h5>
         <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-          Ecommerce Website
+          SPP Payment
         </p>
       </div>
-      <div className="p-6 pt-0">
+      <div className="p-6 pt-5">
         <button
           className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
           type="button"
@@ -98,13 +98,13 @@ export const ProjectSection = () => {
       </div>
       <div className="p-6">
         <h5 className="block font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-          Kessaku Project
+          PKY Profile
         </h5>
         <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
-          Ecommerce Website
+          Semantic Web
         </p>
       </div>
-      <div className="p-6 pt-0">
+      <div className="p-6 pt-5">
         <button
           className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
           type="button"
