ea34ef67f38e41db804f57ff3826b6ca974f3e78
Oyyanv <muhammadroyyan527@gmail.com>
2024-11-29 10:32:37 +0800

    update project

--- src/app/component/project.jsx
+++ src/app/component/project.jsx
@@ -92,7 +92,7 @@ export const ProjectSection = () => {
             data-aos-duration="1600">
       <div className="relative h-18 xl:h-28 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
         <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+          src="https://pky-profile.vercel.app/images/geografi.jpg"
           alt="card-image"
         />
       </div>
