ea261f06e176c3fa7837d6b81013207d03eb273c
Oyyanv <muhammadroyyan527@gmail.com>
2024-12-17 16:02:31 +0800

    Porto Done#2

--- src/app/component/contact.jsx
+++ src/app/component/contact.jsx
@@ -1,53 +1,53 @@
 import React from "react";
-import Image from "next/image";
 export const ContactSection = () =>{
     return(
         <section id="contact" > 
-            <div className="md:mb-10 mb-10 text-4xl text-[#F9F9F9] font-Questrial underline font-semibold justify-center flex" data-aos="fade-up"
+            <div className="md:mb-8 mb-8 text-4xl text-[#F9F9F9] font-Questrial underline font-semibold justify-center flex" data-aos="fade-up"
             data-aos-duration="2000">
             CONTACT
       </div>
-      <div className="bg-white justify-between flex gap-10 items-center p-5 px-10 rounded-3xl overflow-x-hidden overflow-y-hidden">
-      <div>
-          <Image 
-            src="/images/githublogo.png" 
-            width={80} 
-            height={8} 
-            alt="HTML"
-            data-aos="fade-up"
-            data-aos-duration="2000"
+      <div className="bg-white mt-2 justify-between flex gap-10 
+      items-center p-5 px-10 rounded-3xl overflow-x-hidden overflow-y-hidden"
+      data-aos="fade-left"
+      data-aos-duration="1100">
+      <img
+          src="images/email.png"
+          alt="Email"
+          className="w-20 justify-center flex"
           />
-          <p className="mt-2 text-sm font-Questrial flex justify-center">
-            Oyyanv
-          </p>
-        </div>
-      <div>
-          <Image 
-            src="/images/ig.png" 
-            width={80} 
-            height={8} 
-            alt="HTML"
-            data-aos="fade-up"
-            data-aos-duration="2000"
-          />
-          <p className="mt-2 text-sm font-Questrial flex justify-center">
-            oyyn._
-          </p>
-        </div>
-      <div>
-          <Image 
-            src="/images/email.png" 
-            width={80} 
-            height={8} 
-            alt="HTML"
-            data-aos="fade-up"
-            data-aos-duration="2000"
-            className="ml-6"
-          />
-          <p className="mt-2 text-sm font-Questrial flex justify-center">
+          <p className="mt-2 xl:text-xl text-xs font-Questrial flex justify-center">
             oyyan527@gmail.com
-          </p>
-        </div>
+          </p>        
+      </div>
+      <div className="bg-white mt-2 justify-between flex gap-10 items-center 
+      p-5 px-10 rounded-3xl overflow-x-hidden overflow-y-hidden"
+      data-aos="fade-left"
+      data-aos-duration="1200">
+      <img
+          src="images/githublogo.png"
+          alt="Github"
+          className="w-20 justify-center flex"
+          />
+          <p className="mt-2 xl:text-xl font-Questrial flex justify-center">
+            <a href="https://github.com/Oyyanv" target="_blank">Oyyanv</a>
+          </p>        
+      </div>
+      <div className=" mt-7 text-4xl text-[#F9F9F9] font-Questrial underline font-semibold justify-center flex" data-aos="fade-up"
+            data-aos-duration="3000">
+            SOCIAL MEDIA
+      </div>
+      <div className="bg-white mt-2 justify-between flex gap-10 items-center p-5 
+      px-10 rounded-3xl overflow-x-hidden overflow-y-hidden"
+      data-aos="fade-left"
+      data-aos-duration="1300">
+      <img
+          src="images/ig.png"
+          alt="Instagram"
+          className="w-20 justify-center flex"
+          />
+          <p className="mt-2 xl:text-xl font-Questrial flex justify-center">
+            <a href="https://www.instagram.com/oyyn._/" target="_blank">oyyn._</a>
+          </p>        
       </div>
         </section>
     )
