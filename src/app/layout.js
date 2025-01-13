8fd42a4b5a78d8217e1780171b57de66cefa2dfb
Oyyanv <muhammadroyyan527@gmail.com>
2025-01-13 14:54:37 +0800

    Change Title

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "My My My Portofolio",
+  title: "Oyyan's Portofolio",
   description: "Portofolio Oyyan Seorang Web Developer Magang 😅",
 };
 
